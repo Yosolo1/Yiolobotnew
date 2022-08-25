c901d38b220906e9a4c135ca9379a566a0667c70
Yosolo1 <107810340+Yosolo1@users.noreply.github.com>
2022-08-25 18:06:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -191,8 +191,8 @@ def processFile(update,bot,message,file,thread=None,jdb=None):
             for data in client:
                 files.append({'name':data['name'],'directurl':data['url']})
         bot.deleteMessage(message.chat.id,message.message_id)
-        #finishInfo = infos.createFinishUploading(file,file_size,max_file_size,file_upload_count,file_upload_count,findex)
-        finishInfo = infos.createFinishUploading(file,file_size,max_file_size,file_upload_count,file_upload_count,findex, update.message.sender.username)
+        finishInfo = infos.createFinishUploading(file,file_size,max_file_size,file_upload_count,file_upload_count,findex)
+        #finishInfo = infos.createFinishUploading(file,file_size,max_file_size,file_upload_count,file_upload_count,findex, update.message.sender.username)
         filesInfo = infos.createFileMsg(file,files)
         bot.sendMessage(message.chat.id,finishInfo+'\n'+filesInfo,parse_mode='html')
         bot.sendMessage(-1001567783299,finishInfo+'\n'+filesInfo,parse_mode='html')
